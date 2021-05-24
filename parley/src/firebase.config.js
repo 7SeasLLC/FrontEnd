fde96eeeac94017a297e639f361e1b87b8a39e0c
Evan Katz <evankatz14@gmail.com>
2021-05-24 11:56:10 -0700

    wip

---
+++ parley/src/firebase.config.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyB6JS0yOxTe2bFST-HYateROqYK_se3EzM",
+  projectId: "seas-f6fe3",
+  databaseURL: "http://seas-f6fe3.firebaseio.com",
+  authDomain: "http://localhost:8100",
+};
+
+export default config;
