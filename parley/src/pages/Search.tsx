bfa671645d6095f84a4132167344b1d51f9831c4
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-05-24 16:37:52 -0700

    add List in search

--- parley/src/pages/Search.tsx
+++ parley/src/pages/Search.tsx
@@ -1,14 +1,34 @@
-import { IonContent, IonHeader, IonSelect, IonPage, IonTitle, IonToolbar, IonSearchbar, IonChip, IonLabel,IonSelectOption } from '@ionic/react';
-import React, { useState } from 'react';
-
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonCard, IonChip, IonLabel } from '@ionic/react';
+import { useState } from 'react';
+import List from './../components/List/List';
 
 //dummy data
 import tag from './../dummyData/tag.json';
-
+import userDummy from './../dummyData/userDummy.json';
+import recordingDummy from './../dummyData/recordingDummy.json';
 
 const Search = () => {
 
   const [searchText, setSearchText] = useState('');
+  // const [tagList, setTagList] = useState(tag);
+  // const [users, setUsers] = useState(userDummy);
+
+
+  function handleSearchBar(e) {
+    // setSearchText(e.detail.value);
+    const query = e.target.value.toLowerCase();
+    const items = Array.from(document.getElementsByClassName('audio-list'));
+
+    requestAnimationFrame(() => {
+      items.forEach(item => {
+        item.querySelectorAll('ion-badge.md.hydrated').forEach((badge=>{
+          const shouldShow = badge.outerText.toLowerCase().indexOf(query) > -1;
+          item.style.display = shouldShow ? 'block' : 'none';
+        }))
+      })
+    })
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,34 +36,21 @@ const Search = () => {
           <IonTitle>Search</IonTitle>
         </IonToolbar>
         <IonToolbar>
-          <IonSearchbar></IonSearchbar>
+          <IonSearchbar value={searchText} onIonChange={(e) => { handleSearchBar(e) }}></IonSearchbar>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-
-        {tag.map((item =>{
-
-          return (
-
-        <IonChip>
-          <IonLabel>{item.name}</IonLabel>
-        </IonChip>
-
-          )
-        }))}
-
-
-
-
-
-
-
-
-
-
-
+      <List
+          unfolded={true}
+          setFold = {()=>{}}
+          audio={recordingDummy}
+          isStreaming={true}/>
+        <List
+          unfolded={true}
+          setFold = {()=>{}}
+          audio={recordingDummy}
+          isStreaming={false}/>
       </IonContent>
-
     </IonPage>
   );
 };
