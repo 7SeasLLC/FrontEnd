b56d38a42f5cfdad2eaf90e16a0bacc850afd5c6
Evan Katz <evankatz14@gmail.com>
2021-05-24 11:56:10 -0700

    wip

--- parley/src/App.tsx
+++ parley/src/App.tsx
@@ -1,6 +1,11 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuthProvider, FirebaseAuthConsumer } from "@react-firebase/auth";
+import { FirebaseAuthentication } from '@ionic-native/firebase-authentication'
+import config from './firebase.config.js';
 import Feed from './pages/Feed';
 
 /* Core CSS required for Ionic components to work properly */
@@ -23,18 +28,45 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 const App = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/feed">
-          <Feed />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/feed" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+  <FirebaseAuthProvider firebase={firebase} {...config}>
+
+    <FirebaseAuthConsumer>
+      {({ isSignedIn, user, providerId }) => {
+
+        return isSignedIn ?
+          (
+          <IonApp>
+              <IonReactRouter>
+                <IonRouterOutlet>
+                  <Route exact path="/feed">
+                    <Feed />
+                  </Route>
+                  <Route exact path="/">
+                    <Redirect to="/feed" />
+                  </Route>
+                </IonRouterOutlet>
+              </IonReactRouter>
+            </IonApp>
+        ) : (
+          <IonApp>
+            <button
+              onClick={
+                () => {
+                  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                  FirebaseAuthentication.auth().signInWithPopup(googleAuthProvider);
+                }
+              }
+            >
+              Sign in with Google!
+            </button>
+          </IonApp>
+        )
+
+
+      }}
+    </FirebaseAuthConsumer>
+
+  </FirebaseAuthProvider>
 );
 
 export default App;
