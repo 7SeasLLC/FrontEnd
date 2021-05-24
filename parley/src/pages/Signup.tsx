53a82738a0e195fb36147f67eb6f3e279e59fbf8
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-05-24 10:49:24 -0700

    add href to button

--- parley/src/pages/Signup.tsx
+++ parley/src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 
 import { IonContent, IonHeader, IonPage, IonTitle, IonGrid, IonRow, IonCol, IonButton, IonInput, IonCard, IonItem, IonLabel, IonIcon } from '@ionic/react';
-// import { google } from 'ionicons/icons';
+// import { googlePlus } from 'ionicons/icons';
 
 const Signup = () => {
   return (
@@ -11,27 +11,19 @@ const Signup = () => {
             <img id="logo-sign-page" src="assets/logo.png" alt="parley-logo" />
           </IonRow>
           <IonRow className="ion-justify-content-center">
-            Tag Line
+            Tag Line/objective
           </IonRow>
-          <IonRow className="ion-justify-content-center">
-            <IonCol size-lg="3" >
-              <IonLabel>Username or E-mail</IonLabel>
-              <IonInput className="sign-up" value="" placeholder="" onIonChange={e => console.log("")}></IonInput>
-              <IonLabel>{`Password (6 or more characters)`}</IonLabel>
-              <IonInput className="sign-up" type="password" value="" placeholder="" onIonChange={e => console.log("")}></IonInput>
 
-            </IonCol>
-          </IonRow>
           <IonRow className="ion-justify-content-center">
             <IonCol className="ion-text-center" size-lg="3">
-              <p id="term-condition">{`By clicking Agree & Join, you agree to the Parley User Agreement, Privacy Policy, and Cookie Policy.`}</p>
-              <IonButton expand="block" >{`Agree & Join`}</IonButton>
+              <IonButton expand="block" >Sign in with facebook</IonButton>
+              or
             </IonCol>
           </IonRow>
           <IonRow className="ion-justify-content-center">
             <IonCol className="ion-text-center" size-lg="3" >
-              <IonButton expand="block" color="dark" >
-                {/* <IonIcon icon={google} ></IonIcon> */}
+              <IonButton expand="block" color="dark" href="" >
+                {/* <IonIcon icon={googlePlus} ></IonIcon> */}
                 Sign in with Google
               </IonButton>
             </IonCol>
