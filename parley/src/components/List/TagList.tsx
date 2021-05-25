a31fcd19d2b26206c27b9152a61c21f83996917d
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-05-24 23:59:39 -0700

    add margin for search chip

--- parley/src/components/List/TagList.tsx
+++ parley/src/components/List/TagList.tsx
@@ -2,18 +2,21 @@ import { IonCard, IonChip, IonLabel, IonCardHeader } from '@ionic/react';
 
 const TagList = ({ tagList }) => {
 
+
   return (
     <IonCard>
       <IonCardHeader>
         Search By Tags
           </IonCardHeader>
-      {tagList.map(tag => {
-        return (
-          <IonChip color="primary" className="search-tag search-list" id={tag.name}>
-            <IonLabel>{tag.name}</IonLabel>
-          </IonChip>
-        )
-      })}
+      <p className="search-chips">
+        {tagList.map(tag => {
+          return (
+            <IonChip slot="center" color="primary" className="search-tag search-list" id={tag.name}>
+              <IonLabel>{tag.name}</IonLabel>
+            </IonChip>
+          )
+        })}
+      </p>
     </IonCard>
   );
 };
