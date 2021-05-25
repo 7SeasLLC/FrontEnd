9e8f89021960626cf029156d1b148a9145d010a4
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-25 12:52:37 -0700

    Added url to user profile on profile images

--- parley/src/components/List/List.tsx
+++ parley/src/components/List/List.tsx
@@ -3,7 +3,6 @@ import { chevronDownOutline, chevronUpOutline, playOutline } from 'ionicons/icon
 
 import './List.css';
 
-
 const List = ({ unfolded, setFold, audio, isStreaming }) => {
 
   const string = isStreaming ? 'stream': 'recording';
@@ -25,46 +24,71 @@ const List = ({ unfolded, setFold, audio, isStreaming }) => {
     <IonCard>
       <IonItem onClick={() => setFold(string)}>
         <IonCardTitle>
-          {isStreaming ? <h2>Live Streams</h2>: <h2>Recordings</h2>}
+          {isStreaming ? (
+            <h2>Live Streams</h2>
+          ) : (
+            <h2>Recordings</h2>
+          )}
         </IonCardTitle>
-          {unfolded ? <IonIcon icon={chevronUpOutline} slot="end"></IonIcon>: <IonIcon icon={chevronDownOutline} slot="end"></IonIcon>}
+          {unfolded ? (
+            <IonIcon icon={chevronUpOutline} slot="end"></IonIcon>
+          ) : (
+            <IonIcon icon={chevronDownOutline} slot="end"></IonIcon>
+          )}
       </IonItem>
-    {unfolded ? <IonList>
-      {audio.map((item) => {
-        if (item.isStreaming === isStreaming) {
-          return (
-            <IonItemSliding id={item.recording_id} key={item.recording_id} onClick={() => handleClick(item.recording_id)}>
-              <IonItemOptions side="end" >
-                <IonItemOption>
-                  <IonButton icon-only className="listplaybtn" href={`/${isStreaming ? 'listen' : 'play'}/${item.recording_id}`}>
-                    <IonIcon icon={playOutline} />
-                  </IonButton>
-                </IonItemOption>
-              </IonItemOptions>
-                <IonItem lines="none">
-                  <IonAvatar slot="start">
-                    <a href="#top"><img alt={`${item.username}'s avatar`} src={item.profile_img} /></a>
-                  </IonAvatar>
-                  <IonLabel>
-                    <IonCardSubtitle>
-                      {item.username}
-                    </IonCardSubtitle>
-                    <IonCardTitle>
-                      {item.title}
-                    </IonCardTitle>
-                    {item.tags.map((tag) => (
-                      <IonBadge key={tag}>
-                        {tag}
-                      </IonBadge>
-                    ))}
-                  </IonLabel>
-                </IonItem>
+    {unfolded ? (
+      <IonList>
+        {audio.map((item) => {
+          if (item.isStreaming === isStreaming) {
+            return (
+              <IonItemSliding
+                id={item.recording_id}
+                key={item.recording_id}
+                onClick={() => handleClick(item.recording_id)}
+              >
+                <IonItemOptions side="end" >
+                  <IonItemOption>
+                    <IonButton
+                      icon-only
+                      className="listplaybtn"
+                      href={`/${isStreaming ? 'listen' : 'play'}/${item.recording_id}`}
+                    >
+                      <IonIcon icon={playOutline} />
+                    </IonButton>
+                  </IonItemOption>
+                </IonItemOptions>
+                  <IonItem lines="none">
+                    <a
+                      href={'/user/' + item.username}
+                      style={{marginRight: '11px'}}>
+                      <IonAvatar slot="start">
+                        <img
+                          alt={`${item.username}'s avatar`}
+                          src={item.profile_img}
+                        />
+                      </IonAvatar>
+                    </a>
+                    <IonLabel>
+                      <IonCardSubtitle>
+                        {item.username}
+                      </IonCardSubtitle>
+                      <IonCardTitle>
+                        {item.title}
+                      </IonCardTitle>
+                      {item.tags.map((tag) => (
+                        <IonBadge key={tag}>
+                          {tag}
+                        </IonBadge>
+                      ))}
+                    </IonLabel>
+                  </IonItem>
 
-            </IonItemSliding>
-          );
-        }
-      })}
-    </IonList>: null}
+              </IonItemSliding>
+            );
+          }
+        })}
+      </IonList>
+    ) : null}
     </IonCard>
   );
 };
