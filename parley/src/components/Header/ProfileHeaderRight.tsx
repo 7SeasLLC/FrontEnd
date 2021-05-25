eeb3d810b92c2d3203b8ecb4b0dc0ec387cbed40
Ben Pack <benpack007@gmail.com>
2021-05-24 20:02:24 -0700

    Changed header to include edit button on profile.
    Co-authored-by: Ting-Yu Chen <7ingyu@gmail.com>

---
+++ parley/src/components/Header/ProfileHeaderRight.tsx
@@ -0,0 +1,50 @@
+import { IonItem, IonAvatar, IonIcon, IonButton, IonChip, IonModal, IonTextarea } from '@ionic/react';
+import { buildOutline , close } from 'ionicons/icons';
+import { Fragment, useState, useEffect } from 'react';
+import './Headers.css'
+
+const ProfileHeaderRight = ({ user }) => {
+  const [showModal, setShowModal] = useState(false)
+  const [userBio, setUserBio] = useState(user.bio)
+  const [newBioSubmitted, setNewBioSubmitted] = useState(false)
+
+  const saveNewBio = (userBio) => {
+    setNewBioSubmitted(true)
+    setShowModal(false)
+  }
+
+  useEffect(() => {
+
+  }, [newBioSubmitted])
+
+  return (
+    <>
+      <IonButton
+        icon-only className="editButton"
+        slot="end"
+        onClick={() => setShowModal(true)}
+      >
+        <IonIcon className="editIcon" icon={buildOutline}/>
+      </IonButton>
+      <IonModal isOpen={showModal}>
+      <IonButton onClick={() => setShowModal(false)}>
+           <IonIcon icon={ close } />
+        </IonButton>
+        <p className="editBio"> Edit Bio </p>
+        <IonTextarea
+          className="editBioText"
+          rows={6}
+          maxlength={300}
+          value={userBio}
+          onIonChange={e => setUserBio(e.detail.value!)}
+        >
+        </IonTextarea>
+          <IonButton onClick={saveNewBio}>
+            Save
+          </IonButton>
+      </IonModal>
+    </>
+  );
+}
+
+export default ProfileHeaderRight;
