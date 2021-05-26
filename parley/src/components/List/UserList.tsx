94d6fc5cec8e7d16bd6bd13757ae3008e7445a26
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-05-26 00:58:52 -0700

    add link to show list of user

--- parley/src/components/List/UserList.tsx
+++ parley/src/components/List/UserList.tsx
@@ -1,26 +1,26 @@
-import { IonCard, IonChip, IonLabel, IonItem, IonAvatar } from '@ionic/react';
+import { IonCard, IonLabel, IonItem, IonAvatar } from '@ionic/react';
 
 const UserList = ({ users }) => {
 
   return (
     <>
-      <IonCard>
-        {
-          users.map(user => {
-            return (
-              <IonItem>
-                <IonAvatar>
-                  <img src={user.profile_img} alt={`avatar-${user.username}`} />
-                </IonAvatar>
-                <IonLabel className="user-search-list">
-                  <h3>{user.username}</h3>
-                  <p>{`${user.bio.slice(0, 40)}...`}</p></IonLabel>
-              </IonItem>
-            )
-          })
-        }
-      </IonCard>
-    </>
+        <IonCard>
+          {
+            users.map(user => {
+              return (
+                <IonItem button={true} href={'/user/' + user.username} >
+                  <IonAvatar>
+                    <img src={user.profile_img} alt={`avatar-${user.username}`} />
+                  </IonAvatar>
+                  <IonLabel className="user-search-list">
+                    <h3>{user.username}</h3>
+                    <p>{`${user.bio.slice(0, 40)}...`}</p></IonLabel>
+                </IonItem>
+              )
+            })
+          }
+        </IonCard>
+      </>
   );
 };
 
