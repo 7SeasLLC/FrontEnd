d252ef0e4f8914a5a7fb712ef9e3b8b617d57b43
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-27 07:32:47 -0700

    Profile now shows info from local storage

--- parley/src/pages/Profile.tsx
+++ parley/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import { IonPage, IonHeader, IonToolbar, IonContent } from '@ionic/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import axios from 'axios';
 import './Profile.css';
 import data from '../dummyData/userDummy.json'
@@ -7,6 +7,7 @@ import recordingDummy from './../dummyData/recordingDummy.json';
 import Header from '../components/Header/Header';
 import List from './../components/List/List';
 import ProfileInfo from './../components/UserProfile/ProfileInfo'
+import FeedHeaderRight from '../components/Header/FeedHeaderRight'
 import ProfileHeaderRight from '../components/Header/ProfileHeaderRight';
 
 const Profile = ({ match, handleThemeChange }) => {
@@ -14,10 +15,8 @@ const Profile = ({ match, handleThemeChange }) => {
   let ownPage = match ? (match.params.id
     ) : (JSON.parse(window.localStorage.getItem('user')));
 
-  const [user, setUser] = useState(JSON.parse(window.localStorage.getItem('user')));
-  const [userData, setUserData] = useState(data[0]);
-
-  const [recIsOpen, setRecIsOpen] = useState(true);
+  const [userInfo, setUserInfo] = useState(JSON.parse(window.localStorage.getItem('user')));
+  const ownInfo = JSON.parse(window.localStorage.getItem('user'));
 
   const handleSwitch = () => {
       setRecIsOpen(!recIsOpen);
@@ -27,26 +26,26 @@ const Profile = ({ match, handleThemeChange }) => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <Header user={userData}
-                  HeaderRight={ProfileHeaderRight}
+          <Header user={ownInfo}
+                  HeaderRight={match ? FeedHeaderRight : ProfileHeaderRight}
                   backBtn={true}
                   handleThemeChange={handleThemeChange}
           />
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <ProfileInfo userInfo={userData} />
+        <ProfileInfo userInfo={userInfo} />
         <List
           unfolded={true}
           setFold = {() =>{}}
           isStreaming={true}
-          user={user.authId}
+          user={userInfo.authId}
           showTitle={false}/>
         <List
           unfolded={true}
-          setFold = {handleSwitch}
+          setFold = {()=>{}}
           isStreaming={false}
-          user={user.authId}
+          user={userInfo.authId}
           showTitle={false}/>
       </IonContent>
     </IonPage>
