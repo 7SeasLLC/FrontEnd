0cae2709b2701b16a7a7c74da2f12ac06f45091b
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-26 19:41:31 -0700

    Added profile image, username, and listener count

--- parley/src/components/SessionInfo.tsx
+++ parley/src/components/SessionInfo.tsx
@@ -1,10 +1,24 @@
-import { IonItem } from '@ionic/react';
+import { IonCard, IonCardHeader, IonAvatar, IonCardTitle, IonCardContent, IonItem } from '@ionic/react';
 
 const SessionInfo = ({ listeners }) => {
+  const userInfo = JSON.parse(window.localStorage.getItem('user'));
+
   return (
+    <IonCard>
+    <IonCardHeader>
+      <IonAvatar className="profileImg" >
+        <img src={userInfo.photoUrl} alt="Your Profile"></img>
+      </IonAvatar>
+      <IonCardTitle className="profileUsername">
+          {userInfo.username}
+        </IonCardTitle>
+    </IonCardHeader>
+    <IonCardContent>
     <IonItem>
       In Room: {listeners}
     </IonItem>
+    </IonCardContent>
+    </IonCard>
   );
 };
 
