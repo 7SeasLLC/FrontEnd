73a58289799693c636286e20e9a87d574c1913ec
Sidney <sid.nguyen@gmail.com>
2021-05-28 10:08:20 -0700

    refactored host to read string vs array

--- parley/src/components/SessionInfo.tsx
+++ parley/src/components/SessionInfo.tsx
@@ -1,18 +1,6 @@
 import { IonCard, IonCardHeader, IonAvatar, IonCardTitle, IonCardContent, IonItem } from '@ionic/react';
 
 const SessionInfo = ({ listeners, title, host, description, uptime, hostPhoto }) => {
-  var firstHost;
-  var allHosts;
-  if (host) {
-    firstHost = host[0]
-    host.map((each) => {
-      if (allHosts !== undefined) {
-        allHosts = allHosts + ' ' + each
-      } else {
-        allHosts = each
-      }
-    })
-  }
 
   return (
     <IonCard>
@@ -21,7 +9,7 @@ const SessionInfo = ({ listeners, title, host, description, uptime, hostPhoto })
         <img src={hostPhoto} alt="Your Profile"></img>
       </IonAvatar>
       <IonCardTitle className="profileUsername">
-          {firstHost}
+          {host}
         </IonCardTitle>
     </IonCardHeader>
     <IonCardContent>
@@ -32,7 +20,7 @@ const SessionInfo = ({ listeners, title, host, description, uptime, hostPhoto })
       Description: {description}
     </IonItem>
     <IonItem>
-      Hosts: {allHosts}
+      Hosts: {host}
     </IonItem>
     <IonItem>
       In Room: {listeners}
