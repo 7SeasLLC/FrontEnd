3cb8c8124eaafad24839c163f159765fc944b633
Sidney <sid.nguyen@gmail.com>
2021-05-28 11:59:16 -0700

    made separate useEffect for host determination

--- parley/src/pages/Session.tsx
+++ parley/src/pages/Session.tsx
@@ -87,11 +87,13 @@ const Session = (props) => {
     }
   }
 
+  useEffect(() => {
+    determineHost();
+  }, [])
+
   const userInfo = JSON.parse(window.localStorage.getItem('user'));
 
   useEffect (() => {
-    determineHost()
-    console.log(sessionInfo)
     if (roomExists) {
       let chunks = []
       socket.emit('rendered');
@@ -155,7 +157,7 @@ const Session = (props) => {
           socket.emit('join-room', roomId, userId);
       })
     }
-  }, [])
+  }, [host])
 
   const startRecording = () => {
     mediaRecorder.start();
